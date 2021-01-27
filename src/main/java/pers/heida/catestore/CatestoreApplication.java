80d29935c2590f6cc7747226ec1ce428cf7b627c
QingyuMeng <57242255+11192777@users.noreply.github.com>
2021-01-27 15:03:30 +0800

    Build project.

---
+++ src/main/java/pers/heida/catestore/CatestoreApplication.java
@@ -0,0 +1,13 @@
+package pers.heida.catestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pers/heida/catestore/CatestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.heida.catestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
